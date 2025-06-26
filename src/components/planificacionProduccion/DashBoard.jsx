a6700832a9e15e74da21024ebcd2fd87929f749f
JonatanUmul <166423421+JonatanUmul@users.noreply.github.com>
2025-06-26 08:43:44 -0600

    Update DashBoard.jsx

--- src/components/planificacionProduccion/DashBoard.jsx
+++ src/components/planificacionProduccion/DashBoard.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import TablasBoard from './TablasBoard';
 import Issues from './Isuues/ConsultarIssues';
 // import TempHornosSolantec from '../dashbords/TempSolantec'
-import { Carousel } from 'react-bootstrap';
+// import { Carousel } from 'react-bootstrap';
 import './App.css';
 
 const App = () => {
@@ -10,28 +10,28 @@ const App = () => {
     
     <>
       <div  style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
-        <Carousel
+        {/* <Carousel
           interval={300000}
           prevIcon={<span className="carousel-control-prev-icon" aria-hidden="true" />}
           nextIcon={<span className="carousel-control-next-icon" aria-hidden="true" />}
           indicators={false}
-        >
-          <Carousel.Item>
+        > */}
+          {/* <Carousel.Item> */}
             <div style={{  height: '100vh',  justifyContent: 'center', alignItems: 'center' }}>
-              <TablasBoard>1</TablasBoard>
+              <TablasBoard/>
             </div>
-          </Carousel.Item>
-          <Carousel.Item>
+          {/* </Carousel.Item> */}
+          {/* <Carousel.Item>
             <div style={{ height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
               <Issues>2</Issues>
             </div>
-          </Carousel.Item>
+          </Carousel.Item> */}
           {/* <Carousel.Item>
             <div style={{ height: '100vw', justifyContent: 'center', alignItems: 'center' }}>
               <TempHornosSolantec>3</TempHornosSolantec>
             </div>
           </Carousel.Item> */}
-        </Carousel>
+        {/* </Carousel> */}
       </div>
     </>
   );

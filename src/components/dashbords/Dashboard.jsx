14f1a087e2023e90111ce8d223ee05247452225d
Jonathan Umul <166423421+JonatanUmul@users.noreply.github.com>
2025-12-16 14:21:35 -0600

    actualización

--- src/components/dashbords/Dashboard.jsx
+++ src/components/dashbords/Dashboard.jsx
@@ -1,25 +1,11 @@
-import React, { useEffect, useState } from 'react';
+
 import './estilos.css';
-import { formatFecha } from "../utilidades/FormatearFecta";
 import Tabla_Temp from './GraficosHornos/Tabla_Temp';
 const Dashboard = () => {
-  /*const [filtros, setFiltros] = useState({
-    turn: 1,
-    horno: 1,
-    fecha_creacion_inicio: formatFecha(new Date()),
-    fecha_creacion_fin: formatFecha(new Date())
-  });
-
-  const handleFiltrosChange = (newFiltros) => {
-    setFiltros(newFiltros);
-  };
-
-*/
+  
   return (
     <div className="row">
-
     <div className="row">
-
     </div> 
     <div className="col mb-5">
     <div style={{ marginBottom: '20px' }}>
@@ -27,9 +13,6 @@ const Dashboard = () => {
     </div>
     </div>
     </div>
-
- 
-    
   );
 };
 
